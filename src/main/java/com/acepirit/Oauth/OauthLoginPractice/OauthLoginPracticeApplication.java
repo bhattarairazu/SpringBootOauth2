26ac5561ff6109d7c5d81a8630d9c3621f6998ba
bhattarairazu <razubhattarai87@gmail.com>
2021-03-29 20:34:54 +0545

    Spring boot Oauth2 Facebook Login

---
+++ src/main/java/com/acepirit/Oauth/OauthLoginPractice/OauthLoginPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.acepirit.Oauth.OauthLoginPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthLoginPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthLoginPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acepirit/Oauth/OauthLoginPractice/OauthLoginPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acepirit.Oauth.OauthLoginPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthLoginPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
